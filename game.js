e453348e831b6c10ddd40711fd409da37bcc3d1b
draporid <123023542+draporid@users.noreply.github.com>
2023-05-25 17:12:26 +0200

    Add files via upload
    Lava update + better water update.

--- game.js
+++ game.js
@@ -14,6 +14,8 @@ const EMPTY = 0;
 const WALL = 1;
 const SAND = 2;
 const WATER = 3;
+const LAVA = 4;
+const STEAM = 5;
 
 let grid = [];
 let currentElement = SAND;
@@ -24,7 +26,9 @@ const elements = {
     [EMPTY]: { color: 'black' },
     [WALL]: { color: 'gray' },
     [SAND]: { color: 'yellow', fallSpeed: 1 },
-    [WATER]: { color: 'blue', fallSpeed: 1, spread: 1 }
+    [WATER]: { color: 'blue', fallSpeed: 1, spread: 1 },
+    [LAVA]: { color: 'red', fallSpeed: 1, spread: 1 },
+    [STEAM]: { color: 'lightgray', riseSpeed: 1 }
 };
 
 function initGrid() {
@@ -42,47 +46,46 @@ function drawCell(x, y) {
 }
 
 function updateGrid() {
-    const order = [];
-    for (let y = 0; y < gridHeight; y++) {
+    for (let y = gridHeight - 1; y >= 0; y--) {
         for (let x = 0; x < gridWidth; x++) {
-            order.push([x, y]);
-        }
-    }
-    order.sort(() => Math.random() - 0.5);
+            const element = grid[x][y];
+            const elProperties = elements[element];
+        
+            if (elProperties.fallSpeed || elProperties.riseSpeed) {
+                let nx = x;
+                let ny = y + (elProperties.fallSpeed || -elProperties.riseSpeed);
 
-    for (const [x, y] of order) {
-        if (elements[grid[x][y]].fallSpeed) {
-            let nx = x;
-            let ny = y + elements[grid[x][y]].fallSpeed;
-
-            if (ny >= 0 && ny < gridHeight && nx >= 0 && nx < gridWidth && grid[nx][ny] === EMPTY) {
-                grid[nx][ny] = grid[x][y];
-                grid[x][y] = EMPTY;
-                drawCell(x, y);
-                drawCell(nx, ny);
-            } else if (ny < gridHeight && grid[x][y] === WATER) {
-                const left = x - 1;
-                const right = x + 1;
-                const waterLeft = left >= 0 && left < gridWidth && grid[left][y] !== WALL && grid[left][ny] === EMPTY;
-                const waterRight = right >= 0 && right < gridWidth && grid[right][y] !== WALL && grid[right][ny] === EMPTY;
-
-                if (waterLeft && waterRight) {
-                    nx = Math.random() < 0.5 ? left : right;
-                } else if (waterLeft) {
-                    nx = left;
-                } else if (waterRight) {
-                    nx = right;
-                }
-
-                if (nx !== x) {
-                    grid[nx][ny] = grid[x][y];
+                if (element === LAVA && grid[nx][ny] === WATER) {
+                    grid[nx][ny] = STEAM;
                     grid[x][y] = EMPTY;
                     drawCell(x, y);
                     drawCell(nx, ny);
+                    continue;
+                }
+
+                if (ny >= 0 && ny < gridHeight && nx >= 0 && nx < gridWidth && grid[nx][ny] === EMPTY) {
+                    grid[nx][ny] = element;
+                    grid[x][y] = EMPTY;
+                    drawCell(x, y);
+                    drawCell(nx, ny);
+                } else if (elProperties.spread) {
+                    // Try to spread out horizontally
+                    const directions = [x - 1, x + 1];
+                    directions.sort(() => Math.random() - 0.5); // Randomize direction
+                    for (let dir of directions) {
+                        if (dir >= 0 && dir < gridWidth && grid[dir][y] === EMPTY) {
+                            grid[dir][y] = element;
+                            grid[x][y] = EMPTY;
+                            drawCell(x, y);
+                            drawCell(dir, y);
+                            break;
+                        }
+                    }
                 }
             }
         }
     }
+
     ctx.drawImage(offscreenCanvas, 0, 0);
 }
 
